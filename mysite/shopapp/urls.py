551d8331adac9f6c1a73ef5e3c578c0dd88616f3
Iryna Brazouskaya <Rinaberezka@gmail.com>
2025-04-06 01:19:40 +0300

    add templates dir

--- mysite/shopapp/urls.py
+++ mysite/shopapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import shop_index, about_us
 
 app_name = "shopapp"
 urlpatterns = [
-
+    path("", shop_index, name="index"),
+    path("about",about_us, name="about"),
 ]
--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,3 +1,15 @@
+from timeit import default_timer
+# from http.client import HTTPResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+# from django.template import loader
 
-# Create your views here.
+
+def shop_index(request):
+    context = {
+        "time_running": default_timer(),
+    }
+    return render(request,'shopapp/shop_index.html', context=context)
+
+def about_us(request):
+    return render(request,"shopapp/about.html")
